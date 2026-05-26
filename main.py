c22bfdd02ef13b22c4205d043744b9c8d2941751
Luigi Sanchez <lsanchez140790@gmail.com>
2026-05-25 22:26:20 -0500

    Añadido soporte CORS para conectar con frontend

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 
-# Modelo de datos que esperamos recibir del Frontend
+# --- INICIO DE LA SOLUCIÓN CORS ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # El asterisco permite que cualquier web se conecte (ideal para pruebas/MVP)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# --- FIN DE LA SOLUCIÓN CORS ---
+
 class EmailRequest(BaseModel):
     texto: str
 
-# Lista básica de palabras clave sospechosas (para el caso de estudio)
 PALABRAS_SOSPECHOSAS = ["urgente", "contraseña", "bloqueado", "banco", "verificar", "cuenta", "ganador"]
 
 @app.post("/analizar")
 async def analizar_correo(correo: EmailRequest):
     texto_minusculas = correo.texto.lower()
-    coincidencias = [palabra for palabra in PALABRAS_SOSPECHOSAS if palabra in texto_minusculas]
+    coincidencias = [palabra for palabra en PALABRAS_SOSPECHOSAS if palabra in texto_minusculas]
     
     if coincidencias:
         return {
